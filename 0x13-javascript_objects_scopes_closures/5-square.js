eeb577a13cc8fff601efcc73fd6a8fb29e27dbc8
Emmanuelkimutai <emmanuelkimutai64@gmail.com>
2022-11-30 23:59:40 +0300

    Square #1

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,24 +1,14 @@
 #!/usr/bin/node
+/*
+    Class Square
+    inherit from Rectangle
+*/
+const Rectangle = require('./4-rectangle');
 
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
   }
+}
 
-  print () {
-    let a = 'X'.repeat(this.width) + '\n';
-    a = a.repeat(this.height);
-    console.log(a.slice(0, -1));
-  }
-
-  rotate () {
-    [this.height, this.width] = [this.width, this.height];
-  }
-
-  double () {
-    [this.height, this.width] = [this.height * 2, this.width * 2];
-  }
-};
+module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/README.md
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -2,3 +2,4 @@ Write an empty class Rectangle that defines a rectangle
 write a class Rectangle taht defines a rectangle
 Write a class Rectangle that defines a rectangle
 Write a class Square thta defines a square and inherits from rectangle of 4-rectangle.js
+Write a class Square that defines a square and inherites from square of 5-square.js
